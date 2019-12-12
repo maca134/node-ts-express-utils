9305f909b20897eb4b980b6e283075939f0626b5
maca134 <maca134@googlemail.com>
2019-12-12 02:43:12 +0000

    ...

--- src/MiddlewareBase.ts
+++ src/MiddlewareBase.ts
@@ -3,12 +3,10 @@ import { Middleware } from './Middleware';
 
 export abstract class MiddlewareBase implements Middleware {
 	protected abstract _requestHandler: RequestHandler;
-	handler() {
-		return (req: Request, res: Response, next: NextFunction) => {
-			if (!this._requestHandler) {
-				return next(new Error(`no request handler for middleware ${this.constructor.name}`));
-			}
-			return this._requestHandler(req, res, next);
-		};
+	handler(req: Request, res: Response, next: NextFunction) {
+		if (!this._requestHandler) {
+			return next(new Error(`no request handler for middleware ${this.constructor.name}`));
+		}
+		return this._requestHandler(req, res, next);
 	}
 }
